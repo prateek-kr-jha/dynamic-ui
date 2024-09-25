52d24f24af17661cceb08e936811fe43dae7f0c2
Prateek <prateekj1211@gmail.com>
2024-09-26 01:17:54 +0530

    added prettier config file

--- dropdown/webpack.config.js
+++ dropdown/webpack.config.js
@@ -1,40 +1,40 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map", // for proper error message
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Dropdown",
-      filename: "index.html",
-      template: "./src/template.html",
-      inject: "body",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        use: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'eval-source-map', // for proper error message
+    devServer: {
+        watchFiles: ['./src/template.html'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Dropdown',
+            filename: 'index.html',
+            template: './src/template.html',
+            inject: 'body',
+        }),
     ],
-  },
-};
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
